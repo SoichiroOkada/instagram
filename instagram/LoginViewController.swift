3b566a11b27339496157a27788b1269f9d28ca8a
soichiro.okada <straycat0914@gmail.com>
2020-12-05 18:56:48 +0900

    gitgnoreまで

---
+++ instagram/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  instagram
+//
+//  Created by 岡田宗一郎 on 2020/12/04.
+//  Copyright © 2020 soichiro.okada. All rights reserved.
+//
+
+import Foundation
---
+++ instagram/PostViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PostViewController.swift
+//  instagram
+//
+//  Created by 岡田宗一郎 on 2020/12/04.
+//  Copyright © 2020 soichiro.okada. All rights reserved.
+//
+
+import Foundation
