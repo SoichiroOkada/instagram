927549bac56dcc80e2c6ca600fc638e2464f4dc2
soichiro.okada <straycat0914@gmail.com>
2020-12-17 19:08:38 +0900

    20201217

--- instagram/CommentViewController.swift
+++ instagram/CommentViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class CommentViewController: UIViewController {
-
+    
+    @IBOutlet weak var commentImputTextField: UITextField!
+    @IBAction func commentSendButton(_ sender: Any) {
+        if commentImputTextField.text == nil{
+            SVProgressHUD.showError(withStatus: "コメントを入力して下さい")
+        }
+        else{
+            
+            // HUDで処理中を表示
+            SVProgressHUD.show()
+            //usernameを取得してコメントと合わせる
+            // 表示名を取得してTextFieldに設定する
+  
+            
+            
+            // HUDを消す
+            
+            
+            // 画面を閉じてタブ画面に戻る
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- instagram/HomeViewController.swift
+++ instagram/HomeViewController.swift
@@ -100,31 +100,10 @@ let postsRef = Firestore.firestore().collection(Const.PostPath).order(by: "date"
     //commentImputbutton
     @objc func handleButton2(_ sender: UIButton, forEvent event: UIEvent) {
          print("DEBUG_PRINT: commentImputButtonボタンがタップされました。")
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "comment")as! CommentViewController
+        self.present(vc, animated: true, completion:nil)
 
-         // タップされたセルのインデックスを求める
-         let touch = event.allTouches?.first
-         let point = touch!.location(in: self.tableView)
-         let indexPath = tableView.indexPathForRow(at: point)
-
-         // 配列からタップされたインデックスのデータを取り出す
-         let postData = postArray[indexPath!.row]
-
-         // likesを更新する
-         if let myid = Auth.auth().currentUser?.uid {
-             // 更新データを作成する
-             var updateValue: FieldValue
-             if postData.isLiked {
-                 // すでにいいねをしている場合は、いいね解除のためmyidを取り除く更新データを作成
-                 updateValue = FieldValue.arrayRemove([myid])
-             } else {
-                 // 今回新たにいいねを押した場合は、myidを追加する更新データを作成
-                 updateValue = FieldValue.arrayUnion([myid])
-             }
-             // likesに更新データを書き込む
-             let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
-             postRef.updateData(["likes": updateValue])
          }
-     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
